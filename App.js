826f0fe61d966e62261b42f378c0f4eacaebf1a2
Christian Jayme <christian.jayme26@gmail.com>
2024-04-07 01:06:42 +0800

    Homescreen day 1

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hahaysssssssssssssssssssss</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <HomeScreen/>
+      </SafeAreaProvider>
+    </Provider>
+    
   );
 }
 
---
+++ components/NavOptions.js
@@ -0,0 +1,46 @@
+
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React from 'react'
+import tw from "tailwind-react-native-classnames";
+import { Icon } from 'react-native-vector-icons/Icon';
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatScreen",
+  },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({item}) => (
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 h-60`}
+        >
+          <View>
+            <Image 
+              style={{width: 120, height: 120, resizeMode: "contain"}}
+              source={{uri: item.image}}
+            />
+          </View>
+          <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+          <Icon name='arrowright' color="white" type="antdesign" />
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions
